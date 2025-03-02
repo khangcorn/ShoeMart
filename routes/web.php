09a123d31ca9a5ddea1c77d335a2f37d3a33237b
KhangCorn <tonykhang995@gmail.com>
2025-03-02 15:24:02 +0700

    Detail User

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductVariantController;
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< HEAD
 Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
 Route::resource('product_variants', ProductVariantController::class);
@@ -42,3 +39,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [UserController::class, 'profile'])->name('profile');
     Route::post('/profile/update-address', [UserController::class, 'updateAddress'])->name('update-address');
 });
+=======
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+>>>>>>> 46e353b3de802e92975c579c15272d3762d20a49
