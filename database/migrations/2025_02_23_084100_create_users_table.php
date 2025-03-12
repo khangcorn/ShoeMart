126f1e8832328f306c8c81429cc180130de9136d
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-03-12 23:37:55 +0700

    Cập nhật timestamp

--- database/migrations/2025_02_23_084100_create_users_table.php
+++ database/migrations/2025_02_23_084100_create_users_table.php
@@ -4,22 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('username', 50)->unique();
-            $table->string('password', 255);
+            $table->string('password_hash', 255);
             $table->string('email', 100)->unique();
             $table->string('phone', 15)->nullable();
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
