3eaa8e57dc3f52828ed6e71e5aefca9ba97c98cf
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-03-14 23:27:11 +0700

    comment

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -101,7 +101,7 @@ class ProductController extends Controller
     
         // Tạo sản phẩm mới
         $product = Product::create($request->only(['name', 'description', 'price', 'price_sale', 'category_id']));
-        dd($product);
+        // dd($product);
         $totalProductStock = 0; // Tổng stock của sản phẩm (tính từ biến thể)
     
         // Lưu ảnh cho sản phẩm chính
@@ -187,7 +187,7 @@ class ProductController extends Controller
         $product->update(['stock' => $totalProductStock]);
     
         return redirect()->route('products.index')->with('success', 'Sản phẩm đã được tạo thành công.');
-       
+     
 
     }
     
